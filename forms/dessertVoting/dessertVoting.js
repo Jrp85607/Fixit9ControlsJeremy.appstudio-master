abc32dbf3e0a4a2ab579496b7cc3946ea201fa66
Jrp85607 <54645651+Jrp85607@users.noreply.github.com>
2020-10-14 14:47:36 -0500

    Initial commit

---
+++ forms/dessertVoting/dessertVoting.js
@@ -0,0 +1,25 @@
+var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
+                  'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
+#testing if this will work
+
+dessertVoting.onshow=function(){
+    drpDesserts.clear()  
+    while (i < dessertList.length : i = 0) {
+      let i = 2
+  /* add a loop here that adds all the desserts in the array 
+     to the dropdown.
+  */
+}
+
+
+drpDesserts.onclick=function(){
+   // this 'if' kicks user out if they  just clicked on control 
+ // but not on one item in the list.
+    if (typeof(s) == "object")   
+      return                    
+    else {  // the user picked something
+        /* add code here to get item user chose and output 
+        it in a label, using a literal.
+        */
+    }
+}
