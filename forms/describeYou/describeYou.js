2b1bcc16f3398c80b3a7f84535aaed723e3cbea6
Jrp85607 <54645651+Jrp85607@users.noreply.github.com>
2020-10-14 15:29:16 -0500

    version 1

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,3 +1,12 @@
+/*
+Second Program
+1. radio button(s) under the label.
+2. Radio button choices stacked: Optimistic, Pessimistic, Trusting, Envious.
+3. event handler code  that shows message in label control using format: 
+    "I would agree that you are a 'trusting' person too!"
+4. button on the right bottom corner that goes to favExercises form. 
+*/
+
 
 
 
--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -1,25 +1,31 @@
 var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
                   'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
-#testing if this will work
 
 dessertVoting.onshow=function(){
-    drpDesserts.clear()  
-    while (i < dessertList.length : i = 0) {
-      let i = 2
+  drpDesserts.clear()
+  for (y = 0; y <= dessertList.length - 1; y++) {
+    drpDesserts.addItem(dessertList[y])
+    }
   /* add a loop here that adds all the desserts in the array 
      to the dropdown.
   */
 }
 
 
-drpDesserts.onclick=function(){
+drpDesserts.onclick=function(s){
    // this 'if' kicks user out if they  just clicked on control 
  // but not on one item in the list.
     if (typeof(s) == "object")   
       return                    
     else {  // the user picked something
+      drpDesserts.value = s
+    lblPrompt.value = `You picked ${s}, that is a great choice!`
         /* add code here to get item user chose and output 
         it in a label, using a literal.
         */
     }
+}
+
+btn.onclick=function(){
+  ChangeForm(describeYou)
 }
